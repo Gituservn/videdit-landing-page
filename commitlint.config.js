10421b6c8b23dad58a3a6d435560759f593f4607
Evgeniy Serdyuk <91850129+Gituservn@users.noreply.github.com>
2025-06-26 01:19:13 +0300

    feat: initial project setup

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+// commitlint.config.js
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [2, 'always', []],
+    'scope-empty': [2, 'always'],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'build', 'ci', 'revert'],
+    ],
+  },
+};
