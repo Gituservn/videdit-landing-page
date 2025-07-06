4c465463e5f59ebc72fbb46ef8ac59a0e56168f5
IraMira333 <irinat333@gmail.com>
2025-07-06 22:22:10 +0300

    feat: created about section
    removed images to assets

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -35,6 +35,17 @@ export const ui = {
       "Показуйте себе як професіонала або бренд через естетику, зміст і монтаж, що захоплює.",
     quote:
       "Відеомонтаж — це не просто робота, а мистецтво розповідати історії кадрами. Я створюю відео, які захоплюють, надихають і працюють на результат.",
+    aboutSection: "Про мене",
+    aboutTitle:
+      "Привіт! Я Оля — спеціалістка з відеомонтажу. Перетворюю ідеї на динамічні ролики, що захоплюють з перших секунд. У кожному кадрі — сенс, у кожному проєкті — результат.",
+    aboutText1:
+      "Я не просто ріжу відео — я створюю настрій, емоцію та логіку розповіді. \nПрацюю з бізнесом, брендами, контент-мейкерами, і людьми, які хочуть зберегти важливі моменти життя у форматі, до якого приємно повертатися.",
+    aboutText2:
+      "Я віддаю перевагу мінімалістичному та легкому стилю, без зайвих деталей – головне, щоб відео передавало правильний настрій",
+    approach: "мій підхід",
+    approachItem1: "Чіткість і повага до дедлайнів",
+    approachItem2: "Завжди на зв’язку, відкрито обговорюю всі побажання",
+    approachItem3: "Післямонтажне обслуговування: при потребі",
   },
   en: {
     logo: "Olga video",
@@ -62,5 +73,16 @@ export const ui = {
       "Show up as a brand or expert through visuals that inspire, aesthetics that impress, and edits that speak for you.",
     quote:
       "Video editing is not just a job, it's the art of telling stories with footage. I create videos that captivate, inspire, and work for results.",
+    aboutSection: "about me",
+    aboutTitle:
+      "Hi! I'm Olya — a video editor who turns ideas into dynamic videos that captivate from the first seconds. Every frame has meaning, every project — a clear result.",
+    aboutText1:
+      "I don’t just cut video — I create mood, emotion, and a story that flows. \nI work with businesses, brands, content creators, and people who want to preserve meaningful moments in a format they’ll enjoy coming back to.",
+    aboutText2:
+      "I prefer a minimalist, clean style — no unnecessary details, just the right mood conveyed through each scene.",
+    approach: "my approach",
+    approachItem1: "Clarity and respect for deadlines",
+    approachItem2: "Always in touch — I openly discuss all your ideas",
+    approachItem3: "Post-editing support, if needed",
   },
 } as const;
