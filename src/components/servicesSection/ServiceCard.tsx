3da3176ff4b187d049e6f42a14e7beba21f7767e
IraMira333 <irinat333@gmail.com>
2025-07-08 23:05:20 +0300

    fix: en text didn't fit in div

--- src/components/servicesSection/ServiceCard.tsx
+++ src/components/servicesSection/ServiceCard.tsx
@@ -15,7 +15,7 @@ export const ServiceCard = ({ item }: { item: ServiceItemProp }) => {
         </video>
       </div>
       <div className="absolute bottom-[94px] left-0 h-[196px] w-full max-w-[350px] p-4 backdrop-blur-[10px] md:bottom-[84px] md:ml-6 md:h-[272px] md:max-w-[410px] md:p-3 lg:ml-10 lg:h-[290px] lg:max-w-[542px] xl:ml-[60px]">
-        <h3 className="font-alumni lowcase mb-4 text-[32px] leading-none font-bold whitespace-pre-line md:text-5xl lg:text-[64px]">
+        <h3 className="font-alumni mb-4 text-[32px] leading-none font-bold whitespace-pre-line lowercase md:text-5xl lg:text-[64px]">
           {item.title}
         </h3>
         <ul className="pl-5">
