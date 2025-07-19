92737cf216cfe89e8df5cfa06401c45c45cf6cc2
IraMira333 <irinat333@gmail.com>
2025-07-19 23:18:24 +0300

    fix: lg size in getCurrentPosition

--- src/utils/getCurrentPosition.ts
+++ src/utils/getCurrentPosition.ts
@@ -3,7 +3,7 @@ import { ResponsivePosition, SinglePosition } from "@/types";
 export const getCurrentPosition = (pos?: ResponsivePosition): SinglePosition => {
   const width = window.innerWidth;
   if (!pos) return {};
-  if (width >= 1024 && pos.desktop) return pos.desktop;
-  if (width >= 768 && pos.tablet) return pos.tablet;
+  if (width >= 1280 && pos.desktop) return pos.desktop;
+  if (width >= 768 && width < 1280 && pos.tablet) return pos.tablet;
   return {};
 };
